724e057398610f8534cee8c1fd86478dac3bc383
haritsrizkalll <rizkal@moneyhub.id>
2021-10-09 19:17:29 +0700

    contact list

---
+++ app/src/main/java/com/example/tugaspapbkelompok3/Adapter.kt
@@ -0,0 +1,28 @@
+package com.example.tugaspapbkelompok3
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class Adapter(private val context : Activity, private val arrayList: ArrayList<Contact>): ArrayAdapter<Contact>
+    (context, R.layout.list_item,arrayList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater : LayoutInflater = LayoutInflater.from(context);
+        val view : View = inflater.inflate(R.layout.list_item, null);
+
+        val name : TextView = view.findViewById(R.id.tv_name);
+        val number : TextView = view.findViewById(R.id.tv_number);
+        val email : TextView = view.findViewById(R.id.tv_email);
+
+        name.text = arrayList[position].name;
+        number.text = arrayList[position].number;
+        email.text = arrayList[position].email;
+
+        return view
+    }
+
+}
